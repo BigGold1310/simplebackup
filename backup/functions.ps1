329125f7afa5827f8435c732943561d829aafbd2
BigGold1310 <cyrill.naef@gmail.com>
2015-12-14 09:33:33 +0100

    Update functions.ps1

--- backup/functions.ps1
+++ backup/functions.ps1
@@ -1,5 +1,7 @@
 # Functions
 
+# Definiere Attributes
+$attribute = [io.fileattributes]::archive
 
 # Funktion für die Ordner auswahl
 function Select-Folder($message='Select a folder', $path = 0) { 
@@ -21,21 +23,112 @@ function CheckIfConfigExist($configPath){
 		return, $true
 	}
 }
+# Überpüft ob Ordner existiert, falls nicht wird er erstellt
+function CheckIfDirectoryExists($mydirectory){
+	if(-not(Test-Path -Path $mydirectory )){
+    	New-Item -ItemType directory -Path $mydirectory -Force
+		# Directory existierte NICHT
+		return, $false
+	}else{
+		return, $true
+	}
+}
 
 
 # Funktionen für das Backup
+function CreateBackupFolder($mytempdestination, $job){
+	# 0 = Full, 1 = Inkremental, 2 = Differential
+	Write-Host $mytempdestination
+	if ($mytempdestination -gt 3){
+		if($job -eq 0){
+				$mytempdestination = $mytempdestination + "\backup\fullbackup"
+				$tempdestinationcount = (Get-ChildItem -Path $mytempdestination -Directory).count
+				$mytempdestination = $mytempdestination + "\fb" + $tempdestinationcount
+				CheckIfDirectoryExists $mytempdestination
+				return, $mytempdestination
+		}elseif($job -eq 1){
+				$mytempdestination = $mytempdestination + "\backup\incremental"
+				$tempdestinationcount = (Get-ChildItem -Path $mytempdestination -Directory).count
+				$mytempdestination = $mytempdestination + "\in" + $tempdestinationcount
+				CheckIfDirectoryExists $mytempdestination
+				return, $mytempdestination
+		}elseif($job -eq 2){
+				$mytempdestination = $mytempdestination + "\backup\differential"
+				$tempdestinationcount = (Get-ChildItem -Path $mytempdestination -Directory).count
+				$mytempdestination = $mytempdestination + "\di" + $tempdestinationcount
+				CheckIfDirectoryExists $mytempdestination
+				return, $mytempdestination
+		}
+	}else{
+		if($job -eq 0){
+				$mytempdestination = $mytempdestination + "backup\fullbackup"
+				$tempdestinationcount = (Get-ChildItem -Path $mytempdestination -Directory).count
+				$mytempdestination = $mytempdestination + "\fb" + $tempdestinationcount
+				CheckIfDirectoryExists $mytempdestination
+				return, $mytempdestination
+		}elseif($job -eq 1){
+				$mytempdestination = $mytempdestination + "backup\incremental"
+				$tempdestinationcount = (Get-ChildItem -Path $mytempdestination -Directory).count
+				$mytempdestination = $mytempdestination + "\in" + $tempdestinationcount
+				CheckIfDirectoryExists $mytempdestination
+				return, $mytempdestination
+		}elseif($job -eq 2){
+				$mytempdestination = $mytempdestination + "backup\differential"
+				$tempdestinationcount = (Get-ChildItem -Path $mytempdestination -Directory).count
+				$mytempdestination = $mytempdestination + "\di" + $tempdestinationcount
+				CheckIfDirectoryExists $mytempdestination
+				return, $mytempdestination
+		}
+	}
+}
+
 function BackupFolder($path, $destination, $mode){
-	Write-Host $path
-	Backup($path, $destination, $mode)
+	# Debuging! Write-Host $path
+	Backup $path $destination $mode
 }
 function Backup($source, $destination, $mode){
 	$files = Get-ChildItem -Path $source -File
 	$folders = Get-ChildItem -Path $source -Directory
 	foreach ($file in $files){
-		Write-Host $file.FullName
+		# Debuging! Write-Host $file.FullName
+		$tempdestination = $file.FullName.Remove(0,$config[0].length)
+		if($mode -eq 2){
+			# KEINE Archiv bits entfernen!
+			If((Get-ItemProperty -Path $file.fullname).attributes -band $attribute){
+				# Wenn archiv bit vorhanden dann
+				
+			}
+		}elseif($mode -eq 0){		
+			$tempfile = $destination + "\" + $file
+			# Full Backup
+			if(Test-Path $tempfile){
+				If((Get-ItemProperty -Path $file.fullname).attributes -band $attribute){
+					# Wenn archiv bit vorhanden dann
+					# Noting to do
+				}else{
+					Set-ItemProperty -Path $file.FullName -Name attributes -Value ((Get-ItemProperty $file.FullName).attributes -BXOR $attribute)
+				}
+			}else{
+				Copy-Item $file.FullName -Destination $destination
+				Set-ItemProperty -Path $file.FullName -Name attributes -Value ((Get-ItemProperty $file.FullName).attributes -BXOR $attribute)
+			}
+		}else{
+			# Archive bit muss entfernt werden!
+			If((Get-ItemProperty -Path $file.fullname).attributes -band $attribute){
+				# Wenn archiv bit vorhanden dann
+			}
+		}
 	}
 	foreach ($folder in $folders){
-		BackupFolder($folder.FullName, $destination, $mode)
+		# Generiere neue Desitnation (Subfolders)
+		$newdestpath = $destination + "\" + $folder.Name
+		
+		# Erstelle Ordner falls noch nicht existiert
+		CheckIfDirectoryExists($newdestpath)
+		
+		# Entferne Bit von Ordner!
+		Set-ItemProperty -Path $folder.FullName -Name attributes -Value ((Get-ItemProperty $folder.FullName).attributes -BXOR $attribute)
+		BackupFolder $folder.FullName $newdestpath $mode
 	}
 }
 
